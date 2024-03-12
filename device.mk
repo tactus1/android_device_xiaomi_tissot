47f39cbc573e63a2c757bc9e9e1ffec6a926ed76
Tactus <142695427+tactus1@users.noreply.github.com>
2024-03-12 23:17:28 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -12,7 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
