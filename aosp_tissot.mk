73a828bfc25a5ed36f7115c021a13fd35ed9981b
Tactus <142695427+tactus1@users.noreply.github.com>
2024-04-10 16:31:57 +0300

    Update aosp_tissot.mk

--- aosp_tissot.mk
+++ aosp_tissot.mk
@@ -8,13 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AOSP stuff.
+# Inherit some common Horizon stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from tissot device
 AB_OTA_UPDATER := true
 TARGET_IS_LEGACY := true
 $(call inherit-product, device/xiaomi/tissot/device.mk)
+
+# Horizon stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+HORIZON_MAINTAINER := Tactus (@mari0mnu)
+HORIZON_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BUILD_INNERTUNE := true
+TARGET_BUILD_VIMUSIC := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
