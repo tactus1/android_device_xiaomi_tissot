ee1bef8ed90705028d566cd44a31265bd0f543e8
Tactus <142695427+tactus1@users.noreply.github.com>
2024-04-15 14:28:56 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/yaap_tissot.mk
 
 COMMON_LUNCH_CHOICES := \
-    yaap_tissot-ap1a-userdebug \
-    yaap_tissot-ap1a-user \
-    yaap_tissot-ap1a-eng
+    yaap_tissot-userdebug \
+    yaap_tissot-user \
+    yaap_tissot-eng
