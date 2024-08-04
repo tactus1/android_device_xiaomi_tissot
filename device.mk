15efa941afaf88a82a902e7c3d8205eba37ca3b9
Raya Mahardika <rayapkun@hlcyn.co>
2024-08-05 00:12:39 +0700

    tissot: Convert to RRO.
    Change-Id: I394a08cf39d9526155cbb52f310420069877038f

--- device.mk
+++ device.mk
@@ -9,10 +9,6 @@ $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
@@ -71,6 +67,12 @@ PRODUCT_PACKAGES += \
 
 # Properties
 -include device/xiaomi/tissot/prop.mk
+
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayTissot
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Sensors
 PRODUCT_COPY_FILES += \
