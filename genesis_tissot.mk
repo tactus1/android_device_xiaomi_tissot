cd76e9750506c26358924b07869717188068a067
tactus1 <tactus1@proton.me>
2024-11-08 07:09:21 +0000

    GenesisOS bringup

--- genesis_tissot.mk
+++ genesis_tissot.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Genesis stuff.
+$(call inherit-product, vendor/genesis/config/common_full_phone.mk)
 
 # Inherit from tissot device
 AB_OTA_UPDATER := true
@@ -18,7 +18,7 @@ $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := genesis_tissot
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
 PRODUCT_MANUFACTURER := Xiaomi
@@ -30,6 +30,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+
+# Genesis stuff
+GENESIS_MAINTAINER := Tactus
 
 # LineageOS Stuffs
 TARGET_GAPPS_ARCH := arm64
